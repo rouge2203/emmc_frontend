a6efc84f8e06437be1f4d34234ab9b4420c8fa83
rouge-mac <joseruizsuarez@hotmail.com>
2025-10-24 00:25:14 -0600

    Integrate Tailwind CSS into the project, update HTML structure, and remove unused CSS and components. Added Tailwind CSS dependencies in package.json and package-lock.json. Updated Vite configuration to include Tailwind CSS plugin. The main application now displays a simple "Hello world!" message styled with Tailwind CSS.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <>
+      <h1 className="text-5xl font-extrabold  underline">Hello world!</h1>
+      <h1 className="text-5xl font-extrabold  underline font-helvetica">
+        Hello world!
+      </h1>
+      <h1 className="text-3xl text-roboto mt-3 font-bold underline">
+        Hello world! this is a test of the roboto font
+      </h1>
+    </>
+  </StrictMode>
+);
